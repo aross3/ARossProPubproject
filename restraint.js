ae215867c0a2afc24179c47c42fffd52c717c8b2
aross3 <allisonsross@gmail.com>
2016-06-14 22:58:58 -0400

    added files

---
+++ restraint.js
@@ -0,0 +1,23 @@
+// function openRS(evt, RSname) {
+//     var i, tabcontent, tablinks;
+
+//     tabcontent = document.getElementsByClassName("tabcontent");
+//     for (i = 0; i < tabcontent.length; i++) {
+//         tabcontent[i].style.display = "none";
+//     }
+
+//     tablinks = document.getElementsByClassName("tablinks");
+//     for (i = 0; i < tablinks.length; i++) {
+//         tablinks[i].className = tablinks[i].className.replace(" active", "");
+//     }
+
+//     document.getElementById(RSname).style.display = "block";
+//     evt.currentTarget.className += " active";
+// }
+
+// document.addEventListener("DOMContentLoaded", function(){
+//     var e = {currentTarget: document.getElementById("first")};
+//     openRS(e,"seatedcradle");
+//     console.log("yo");
+// });
+
